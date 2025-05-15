7fb242b142cf46b02be0e282de7afbd5730f3385
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-15 07:19:10 +0200

    added increment and reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import { useState } from 'react'
+import {Button} from "./Button.tsx";
 
 
 function App() {
   const [count, setCount] = useState(0)
+    const maxCount = 5
 
-  return (
-    <div className={'container'}>
-        <button className={'count'} onClick={() => setCount((count) => count + 1)}>
-          {count}
-        </button>
-        <div className={'btnWrapper'}>
-            <button>inc</button>
-            <button>reset</button>
+    const increment = () => {
+        if (count < maxCount) {
+            setCount(count + 1)
+        }
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
+    return (
+        <div className={'container'}>
+            <Button className={`count ${count === maxCount ? 'max' : ''}`}>
+                {count}
+            </Button>
+            <div className={'btnWrapper'}>
+                <Button
+                    onClick={increment}
+                    disabled={count === maxCount}
+                >increment
+                </Button>
+                <Button
+                    onClick={reset}
+                    disabled={count === 0}
+                >reset
+                </Button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default App
